63b38b5c4de37caf697c6508a6bd38c0be6094c1
Takahiro Hine <thine@c1r1s8.42tokyo.jp>
2024-06-30 11:09:27 +0900

    test2

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,13 +6,13 @@
 /*   By: thine <thine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 13:29:00 by thine             #+#    #+#             */
-/*   Updated: 2024/06/27 15:19:24 by thine            ###   ########.fr       */
+/*   Updated: 2024/06/30 10:57:47 by thine            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-t_list	*ft_lstnew(void *content)
+t_list	*ft_lstnew(void	*content)
 {
 	t_list	*newlst;
 
